906567bf8f73d150f4e3e6a0b46b1a6d4e357326
Douglas Numeriano <douglas_rnmeriano@hotmail.com>
2021-05-27 15:21:55 -0300

    De fucntion para const

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -18,7 +18,7 @@ type ChartData = {
     series: SeriesData[];
 } 
 
-function BarChart(){
+const BarChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({
         labels: {
